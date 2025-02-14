f1761ea019243172e7bcd9dce7e300f792a0275e
Sergio Campos Delgado <campos.delgado.sergio@iescamas.es>
2025-02-14 10:32:08 +0100

    Proyecto Terminado

--- productos/src/main/java/com/productos/App.java
+++ productos/src/main/java/com/productos/App.java
@@ -6,8 +6,15 @@ package com.productos;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    // Método main que mezcla la lógica de negocio con la presentación
+    public static void main(String[] args) {
+        Producto sp = new Producto();
+        sp.addProduct("Monitor", 250);
+        sp.addProduct("Teclado", 50);
+        sp.addProduct("Mouse", 25);
+        sp.addProduct("Impresora", 150); // Debería activar la conversión de arrays a listas
+        sp.listProducts();
+        sp.findProduct("Teclado");
+        sp.findProduct("Scanner"); // Producto inexistente para provocar error
     }
 }

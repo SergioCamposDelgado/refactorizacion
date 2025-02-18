bd770c2338d838222bbdaf6c08b2b9edad931151
Israel Narvaez <narvaez.saavedra.israel@iescamas.es>
2025-02-18 18:26:55 +0100

    Continuacion de las prubas unitarias

--- productos/src/test/java/com/productos/ProductoTest.java
+++ productos/src/test/java/com/productos/ProductoTest.java
@@ -7,27 +7,40 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ProductoTest {
-	Producto product;
-	
+	Producto product1;
+	Producto product2;
 	@BeforeEach
 	void setUp() throws Exception {
-		product = new Producto("Grafica", 350.0);
+		product1 = new Producto("Grafica", 350.0);
+		product2 = new Producto("Procesador", 90.0);
 	}
 	@DisplayName("ToString")
 	@Test
 	void test1() {
-		
+		String resultadoEsperado = "Grafica" + ", " + "350.0" +"€";
+		String resultadoObtenido = product1.toString();
+		assertEquals(resultadoEsperado, resultadoObtenido);
 	}
 	
 	@DisplayName("listProduct")
 	@Test
 	void test2() {
-		
+		String resultadoEsperado = "Grafica" + ", " + "350.0" +"€";
+		String resultadoObtenido = Producto.listProducts();
+		assertEquals(resultadoEsperado, resultadoObtenido);
 	}
 	
 	@DisplayName("compareTo")
 	@Test
 	void test3() {
+		int resultadoEsperado = ;
+		int resultadoObtenido = product1.compareTo(product2);
+		assertEquals(resultadoEsperado, resultadoObtenido);
+	}
+	
+	@DisplayName("findProduct")
+	@Test
+	void test4() {
 		
 	}
 

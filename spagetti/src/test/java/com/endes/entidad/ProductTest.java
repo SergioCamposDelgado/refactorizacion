04076693495fb2cba53d405c4a4c6ba00e5c59c2
Sergio Campos Delgado <campos.delgado.sergio@iescamas.es>
2025-02-25 12:49:58 +0100

    Test de Product realizados por completo

--- spagetti/src/test/java/com/endes/entidad/ProductTest.java
+++ spagetti/src/test/java/com/endes/entidad/ProductTest.java
@@ -40,12 +40,100 @@ class ProductTest {
 		Exception exception_name = assertThrows(IllegalArgumentException.class, ()->new Product(null, 3.20) );
 		String mensajeEsperado_name = "Error: Nombre inválido";
 		
+		Exception exception_name2 = assertThrows(IllegalArgumentException.class, ()->new Product(" ", 3.20) );
+		String mensajeEsperado_name2 = "Error: Nombre inválido";
+		
 		Exception exception_price = assertThrows(IllegalArgumentException.class, ()->new Product("Laptop", -3.20) );
 		String mensajeEsperado_price = "Error: Precio negativo";
 		
 		assertEquals(mensajeEsperado_price, exception_price.getMessage());
+		
 		assertEquals(mensajeEsperado_name, exception_name.getMessage());
 		
+		assertEquals(mensajeEsperado_name2, exception_name2.getMessage());
+		
+	}
+	
+	@Test
+	@DisplayName ("Debe lanzar ser valido si el toString funciona")
+	void testToString() {
+		
+		String mensajeObtenido = product.toString();
+		String mensajeEsperado = "Product [name=" + product.getName() + ", price=" + product.getPrice() + "]";
+		
+		assertEquals(mensajeEsperado, mensajeObtenido);
+			
+	}
+	
+	@Test
+	@DisplayName ("Debe ser valido el SetPrice() con precio valido")
+	void testSetPrice() {
+		
+		product.setPrice(20.00);
+		double precioObtenido = product.getPrice();
+		double precioEsperado = 20.00;
+		
+		assertEquals(precioObtenido, precioEsperado);
+			
+	}
+	
+	@Test
+	@DisplayName ("Debe crear un excepcion de precio invalido")
+	void testSetPriceExcepcion() {
+		double precioEsperado = 1200.00;
+
+		Exception exception_price = assertThrows(IllegalArgumentException.class, ()->product.setPrice(-20.00) );
+		String mensajeEsperado_price = "Error: Precio negativo";
+		
+		double precioObtenido = product.getPrice();
+		
+		assertEquals(precioObtenido, precioEsperado);
+		assertEquals(mensajeEsperado_price, exception_price.getMessage());
+			
+	}
+	
+	@Test
+	@DisplayName ("Debe ser valido el SetName() con nombre valido")
+	void testSetName() {
+		
+		product.setName("PC");
+		String mensajeObtenido = product.getName();
+		String mensajeEsperado = "PC";
+		
+		assertEquals(mensajeEsperado, mensajeObtenido);
+			
+	}
+	
+	@Test
+	@DisplayName ("Debe crear un excepcion de nombre invalido")
+	void testSetNameExcepcion() {
+		
+		String mensajeEsperado = "Laptop";
+		
+
+		Exception exception_name = assertThrows(IllegalArgumentException.class, ()->product.setName(null) );
+		String mensajeEsperado_exception = "Error: Nombre inválido";
+		
+		Exception exception_name2 = assertThrows(IllegalArgumentException.class, ()->product.setName(" ") );
+		String mensajeEsperado_exception2 = "Error: Nombre inválido";
+		
+		String mensajeObtenido = product.getName();
+		
+		assertEquals(mensajeObtenido, mensajeEsperado);
+		assertEquals(mensajeEsperado_exception, exception_name.getMessage());
+		assertEquals(mensajeEsperado_exception2, exception_name2.getMessage());
+		
+			
+	}
+	
+	@Test
+	@DisplayName ("Debe ser valido el Constructor vacio ()")
+	void testConstructorVacio() {
+		
+		Product p = new Product();
+		
+		assertNotNull(p);
+			
 	}
 	
 	@ParameterizedTest
